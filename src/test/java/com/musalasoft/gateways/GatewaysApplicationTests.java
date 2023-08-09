40773d66059385071c9702bb871327a3f930de13
gagan <gba@logpoint.com>
2023-08-09 09:00:06 +0545

    initial commit

---
+++ src/test/java/com/musalasoft/gateways/GatewaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musalasoft.gateways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
