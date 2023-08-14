ebbe886c9caade3bdf81e96ab5e9d8b1b3a439b0
gagan <gba@logpoint.com>
2023-08-14 20:19:43 +0545

    added validation for gateway

--- src/main/java/com/musalasoft/gateways/dtos/GatewayDTO.java
+++ src/main/java/com/musalasoft/gateways/dtos/GatewayDTO.java
@@ -1,5 +1,6 @@
 package com.musalasoft.gateways.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,11 +13,15 @@ import lombok.Setter;
 @AllArgsConstructor
 public class GatewayDTO {
 
+    @NotBlank(message = "Serial number is not present in request body")
     private String serialNumber;
 
+    @NotBlank(message = "Name is not present in request body")
     private String name;
 
-    @Pattern(regexp = "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b") // IPv4 validation regex
+    @NotBlank(message = "ipv4Address is not present in request body")
+    // IPv4 validation regex
+    @Pattern(regexp = "\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b", message = "Invalid IPv4 address")
     private String ipv4Address;
 
 }
--- src/main/java/com/musalasoft/gateways/service/GatewayService.java
+++ src/main/java/com/musalasoft/gateways/service/GatewayService.java
@@ -77,6 +77,13 @@ public class GatewayService {
 
         if (gatewayEntity.isPresent()) {
             GatewayEntity gateway = gatewayEntity.get();
+
+            if (gateway.getDevices().size() >= 10) {
+                GatewayResponse<String> response = new GatewayResponse<>(400,
+                    "No more than 10 peripheral devices are allowed for a gateway");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            }
+
             List<PeripheralDeviceEntity> peripheralDevices = peripheralDevice.stream()
                 .map(peripheralDeviceDTO -> {
                     PeripheralDeviceEntity peripheralDeviceEntity = new PeripheralDeviceEntity();
